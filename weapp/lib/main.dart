bdca4538b95fbea15240d2f7d61a33a0b3f595d0
lishupeng <lishupeng@credit-first.com.cn>
2019-07-01 11:07:08 +0800

    简单的收藏和取消收藏功能

--- weapp/lib/main.dart
+++ weapp/lib/main.dart
@@ -4,10 +4,15 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  MyAppBar({this.title});
+  final Widget title;
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Welcome to Flutter',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: new RandomWords(),
       // home: new Scaffold(
       //   appBar: new AppBar(
@@ -29,13 +34,17 @@ class RandomWords extends StatefulWidget {
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
   @override
   Widget build(BuildContext context) {
     // final wordPair = new WordPair.random();
     // return new Text(wordPair.asPascalCase);
     return new Scaffold(
-appBar: new AppBar(
+  appBar: new AppBar(
   title: new Text('startup Name Generator'),
+  actions: <Widget>[
+    new IconButton(icon: new Icon(Icons.list),onPressed: _pushSaved,)
+  ],
 ),
 body: _buildSuggestions(),
     );
@@ -53,12 +62,60 @@ body: _buildSuggestions(),
       }
     );
   }
+
   Widget _buildRow(WordPair pair){
+    final alreadySaved =_saved.contains(pair);
     return new ListTile(
        title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
        ),
+       trailing: new Icon(
+         alreadySaved?Icons.favorite:Icons.favorite_border,
+         color:alreadySaved?Colors.red:null,
+       ),
+       onTap: (){
+         setState(() {
+           if(alreadySaved){
+             _saved.remove(pair);
+           }else{
+             _saved.add(pair);
+           }
+         });
+       },
     );
   }
+
+void _pushSaved(){
+Navigator.of(context).push(
+  new MaterialPageRoute(
+    builder: (context){
+      final tiles = _saved.map(
+        (pair){
+          return new ListTile(
+            title: new Text(
+              pair.asPascalCase,
+              style:_biggerFont,
+
+            ),
+          );
+        },
+      );
+      final divided = ListTile.divideTiles(
+        context: context,
+        tiles: tiles,
+      )
+      .toList();
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Saved Suggestions'),
+        ),
+        body: new ListView(children: divided),
+      );
+
+    }
+  )
+);
+}
+
 }
